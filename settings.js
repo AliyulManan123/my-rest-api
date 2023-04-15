e65e840e27734e76ca13a70ca4bcbbe5e1980e0d
AliyulManan <108641435+SarkonTampan@users.noreply.github.com>
2023-04-15 14:26:15 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,27 +1,27 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
+global.creator = 'Ikhsan'// yourname
 global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.ACTIVATION_TOKEN_SECRET = "shanbot" //isi apa aja bebas
+global.your_email = "shanbot20@gmail.com" //email
+global.email_password = "lotlgtbgbgltpxke" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
 	noapikey:{
 		status: 403,
-        message: 'Input parameter apikey',
+        message: 'apikeynya mana deck!',
         creator: `${creator}`,
         result: "error"
     },
     error: {
         status: 503,
-        message: 'Service Unavaible, Sedang dalam perbaikan',
+        message: 'Service tidak tersedia, Sedang dalam perbaikan',
         creator: `${creator}`
     },
     apikey: {
     	status: 403,
-    	message: 'Forbiden, Invalid apikey',
+    	message: 'Terlarang, Invalid apikey',
     	creator: `${creator}`
     },
     noturl: {
